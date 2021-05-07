6d8400fb2b44eecb7f0db342db5a22295a897741
taimoor123-prog <taimorkhan111@gmail.com>
2021-05-07 15:58:02 +0500

    edit

--- index.js
+++ index.js
@@ -1,51 +1,15 @@
-const functions = require("firebase-functions");
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-//const fileUpload = require('express-fileupload');
 var routes = require('./src/routes');
-//var logger = require('morgan');
 require('./src/services/firebase-init');
 const PORT = process.env.PORT || 3000;
 var app = express();
 
-//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/public', express.static(path.resolve(__dirname, 'public')));
-//app.use(fileUpload());
-//app.use(fileParser);
+app.get('/', (req, res) => {res.send("Welcome to SkidsterApp")});
 app.use('/api/v1/', routes);
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-let abc = 0;
-exports.app = functions.https.onRequest(app);
-
-// const functions = require('firebase-functions');
-// const express = require('express');
-// const app = express();
-// app.get('/bigben', (req, res) => {
-//   const date = new Date();
-//   const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
-//   res.send(`
-//       <!doctype html>
-//       <head>
-//         <title>Time</title>
-//         <link rel="stylesheet" href="/style.css">
-//         <script src="/script.js"></script>
-//       </head>
-//       <body>
-//         <p>In London, the clock strikes:
-//           <span id="bongs">${'BONG '.repeat(hours)}</span></p>
-//         <button onClick="refresh(this)">Refresh</button>
-//       </body>
-//     </html>`);
-// });
-
-// app.get('/api', (req, res) => {
-//   const date = new Date();
-//   const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
-//   res.json({ bongs: 'BONG '.repeat(hours) });
-// });
-// exports.app = functions.https.onRequest(app);
+app.listen(PORT, ()=>console.log("http://localhost:3000/"));
--- src/routes/admin/admin.routes.js
+++ src/routes/admin/admin.routes.js
@@ -5,16 +5,16 @@ var router = express.Router();
 
 //Dashboard Reports
 //Users
-router.get('/get-all-users', adminController.getAllUsers);//done
+router.get('/get-all-users', adminController.getAllUsers);
 //Free Plan
-router.get('/get-free-plan', adminController.getFreePlan);//done
-router.get('/update-free-plan', adminController.updateFreePlan);//done
+router.get('/get-free-plan', adminController.getFreePlan);
+router.get('/update-free-plan', adminController.updateFreePlan);
 //Packages
-router.get('/get-all-packages', adminController.getAllPackages);//done
-router.get('/save-package', adminController.savePackage);//done
-router.get('/get-package', adminController.getPackage);//done
-router.get('/update-package', adminController.updatePackage);//done
-router.get('/delete-package', adminController.deletePackage);//done
+router.get('/get-all-packages', adminController.getAllPackages);
+router.get('/save-package', adminController.savePackage);
+router.get('/get-package', adminController.getPackage);
+router.get('/update-package', adminController.updatePackage);
+router.get('/delete-package', adminController.deletePackage);
 
 
 module.exports = router;
